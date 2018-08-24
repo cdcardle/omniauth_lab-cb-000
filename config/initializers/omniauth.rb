b33aa8f75775a66ccd63372380ce51ce7a55225a
Chris Cardle <cdcardle@gmail.com>
2018-08-24 20:34:34 +0000

    automatically backed up by learn

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV['2e6a422a951a68ce2a62'], ENV['fec24d2b6a1c908c73bb479ed211f61548388815']
+  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
 end
